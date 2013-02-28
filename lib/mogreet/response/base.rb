7c1f310affae5f5eac69011ca6f9af073a134780
Amos Lanka <amoslanka@gmail.com>
2013-02-28 11:36:21 -0800

    changed the 0 mogreet status code response to report as InternalErrorException.

--- lib/mogreet/response/base.rb
+++ lib/mogreet/response/base.rb
@@ -21,11 +21,10 @@ module Mogreet::Response
       puts "Mogreet submission responded with an exception: status=#{self.status} code=#{self.code} message=#{self.message}"
       case self.code.to_i
       when 400 then raise Mogreet::RejectedException.new(self.message, self.code)
-      when 0, 403 then raise Mogreet::ForbiddenException.new(self.message, self.code)
+      when 403 then raise Mogreet::ForbiddenException.new(self.message, self.code)
       when 404 then raise Mogreet::UnknownRequestException.new(self.message, self.code)
       when 430..499 then raise Mogreet::InvalidRequestException.new(self.message, self.code)
-      when 500 then raise Mogreet::InternalErrorException.new(self.message, self.code)
-      when 600..601 then raise Mogreet::InternalErrorException.new(self.message, self.code)
+      when 0, 500..601 then raise Mogreet::InternalErrorException.new(self.message, self.code)
       when 602..699 then raise Mogreet::UserException.new(self.message, self.code)
       else
         raise Exception.new(self.message)
